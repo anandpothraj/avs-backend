ccaf287e7b5cebaa26b7e3ea658cb94924544b48
anandpothraj <anandpothraj11052001@gmail.com>
2023-05-10 23:13:38 +0530

    Removed helper.js from userRoute and added to util

---
+++ utils/helper.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+function isFieldPresentInRequest(reqBody, fieldName) {
+  try {
+    return (
+      reqBody.hasOwnProperty(fieldName) &&
+      reqBody[fieldName] !== null &&
+      reqBody[fieldName] !== undefined &&
+      reqBody[fieldName] !== ""
+    );
+  } catch (e) {
+    console.log(`Error while check field name: ${e}`);
+    return false;
+  }
+}
+
+module.exports = {
+  isFieldPresentInRequest,
+};
