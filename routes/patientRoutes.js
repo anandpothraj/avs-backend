2114e745e72dc8dd5153563a4bf20f9a85ee5011
anandpothraj <anandpothraj11052001@gmail.com>
2023-09-06 18:39:36 +0530

    created delete appointement api

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { bookAppointment, fetchAppointments } = require('../api/patient');
+const { bookAppointment, fetchAppointments, deleteAppointment } = require('../api/patient');
 
 router.route('/book/appointment').post(bookAppointment);
 router.route('/fetch/appointments/:id').get(fetchAppointments);
+router.route('/remove/appointment/:id').delete(deleteAppointment);
 
 module.exports = router;
